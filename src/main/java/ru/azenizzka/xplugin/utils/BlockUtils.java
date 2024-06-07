1248e5bb000ada84aa669e30d6fb4c90d038fe21
azenizzka <azenizzka@gmail.com>
2024-06-07 14:01:45 +0700

    fix item durability

--- src/main/java/ru/azenizzka/xplugin/utils/BlockUtils.java
+++ src/main/java/ru/azenizzka/xplugin/utils/BlockUtils.java
@@ -1,48 +1,50 @@
 package ru.azenizzka.xplugin.utils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
 import ru.azenizzka.xplugin.XPlugin;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class BlockUtils {
+	private static final Set<Block> processingBlocks = new HashSet<>();
+
 	public static void breakBlocks(Player player, List<Block> blocks) {
-		ItemStack tool = player.getInventory().getItemInMainHand();
-		Damageable damageable = (Damageable) tool.getItemMeta();
+		synchronized (processingBlocks) {
+			blocks.removeIf(block -> !processingBlocks.add(block));
 
-		int maxDurability = tool.getType().getMaxDurability();
-		int unbreakingCoeff = tool.getEnchantmentLevel(Enchantment.DURABILITY) + 1;
+			ItemStack tool = player.getInventory().getItemInMainHand();
+			Damageable damageable = (Damageable) tool.getItemMeta();
 
+			int unbreakingCoeff = tool.getEnchantmentLevel(Enchantment.DURABILITY) + 1;
+			int maxDamage = damageable.getDamage() + blocks.size() / unbreakingCoeff;
 
-		int maxDamage = damageable.getDamage() + blocks.size() / unbreakingCoeff;
+			if (tool.getType().getMaxDurability() - maxDamage < 0)
+				return;
 
-		if (maxDurability - maxDamage < 0)
-			return;
+			damageable.setDamage(maxDamage);
+			tool.setItemMeta(damageable);
 
-		damageable.setDamage(maxDamage);
-		tool.setItemMeta(damageable);
+			for (int i = 0; i < blocks.size(); i++) {
+				Block block = blocks.get(i);
 
+				if (!block.isPreferredTool(tool))
+					continue;
 
-		for (int i = 0; i < blocks.size(); i++) {
-			Block block = blocks.get(i);
-
-			if (!block.isPreferredTool(tool))
-				continue;
-
-			BukkitTask bukkitTask = new BukkitRunnable() {
-				@Override
-				public void run() {
+				Bukkit.getScheduler().runTaskLater(XPlugin.instance, () -> {
 					block.breakNaturally(tool, true, true);
-				}
-			}.runTaskLater(XPlugin.instance, 3L * i);
+					synchronized (processingBlocks) {
+						processingBlocks.remove(block);
+					}
+				}, 3L * i);
+			}
 		}
-
 	}
 }
-
